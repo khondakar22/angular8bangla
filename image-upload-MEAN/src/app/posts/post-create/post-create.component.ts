4dafd7b2d4176f6efa58d4e8f307087ed28cbedb
Khondakar Readul Islam <khondakar22readul@gmail.com>
2019-09-08 15:43:09 +0200

    Event Binding Added

---
+++ image-upload-MEAN/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model.';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent implements OnInit {
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postedCreated = new EventEmitter<Post>();
+  constructor() {}
+
+  ngOnInit() {}
+
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   this.newPost = postInput.value;
+  // }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+     title: form.value.title,
+     content: form.value.content,
+   };
+    this.postedCreated.emit(post);
+  }
+}
